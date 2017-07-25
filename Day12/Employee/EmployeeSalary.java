11f1dcd9481ad59e7f5c20c97ffa1f1ac73d4ae6
Apurva Ruplag <apurva.ruplag@gmail.com>
2017-07-25 17:36:25 +0530

    Day12

--- Day12/Employee/EmployeeSalary.java
+++ Day12/Employee/EmployeeSalary.java
@@ -21,12 +21,39 @@ public class EmployeeSalary {
         salaryList.add(574.7);
         salaryList.add(3563.546);
 
-        Iterator<Double> iterator = salaryList.iterator();
+        printHRA(salaryList);
+        printList(salaryList);
+        increaseSalary(salaryList);
+        System.out.println();
+        printList(salaryList);
 
+    }
+
+    private static void increaseSalary(ArrayList<Double> salaryList) {
+        Iterator<Double> iterator = salaryList.iterator();
+        int i = 0;
+        while(iterator.hasNext()) {
+            double salary = iterator.next();
+            if (salary < 20000.00){
+                salary += salary * 0.2;
+                salaryList.set(i, salary);
+            }
+            i++;
+        }
+    }
+
+    private static void printHRA(ArrayList<Double> salaryList) {
+        Iterator<Double> iterator = salaryList.iterator();
         while(iterator.hasNext()) {
             System.out.println(iterator.next()*0.3);
         }
+    }
 
+    private static void printList(ArrayList<Double> salaryList) {
+        Iterator<Double> iterator = salaryList.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 
 
