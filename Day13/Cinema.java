46ba8775dc75d6e257929868bb337c6e14024a31
Apurva Ruplag <apurva.ruplag@gmail.com>
2017-07-26 18:01:58 +0530

    Day13

--- Day13/Cinema.java
+++ Day13/Cinema.java
@@ -4,23 +4,36 @@
 public class Cinema {
     public static void main(String[] args) {
         User user = new User(11, "Raj", 0, 7);
+
         try {
+
             MovieService.buy(user, 3000.89);
             MovieService.watch(user);
+
         } catch(PriceInvalidException e) {
+
             System.out.println(e.getMessage());
-        } catch (AgeLessThanTenException e) {
+
+        } catch (AgeOutOfRangeException e) {
+
             System.out.println(e.getMessage());
-        } catch (PriceLessThanTwoThousand e) {
+
+        } catch (PriceOutOfRangeException e) {
+
             System.out.println(e.getMessage());
-        } catch (RatingLessThanThreeException e) {
+
+        } catch (RatingOutOfRangeException e) {
+
             System.out.println(e.getMessage());
+
         } catch (AgeInvalidException e) {
+
             System.out.println(e.getMessage());
-        } catch (AgeLessThanTwentyException e) {
-            System.out.println(e.getMessage());
+
         } catch (Exception e) {
+
             System.out.println(e.getMessage());
+
         }
     }
 }
--- Day13/MovieService.java
+++ Day13/MovieService.java
@@ -3,34 +3,34 @@
  */
 public class MovieService {
 
-    public static void buy(User user, double price) throws PriceInvalidException, PriceLessThanTwoThousand, AgeInvalidException, AgeLessThanTenException, AgeLessThanTwentyException, RatingLessThanThreeException{
+    public static void buy(User user, double price) throws PriceInvalidException, PriceOutOfRangeException, AgeInvalidException, AgeOutOfRangeException, RatingOutOfRangeException {
         if (price <= 0) {
             throw new PriceInvalidException("Price cannot be less than or equal to zero");
         }
 
         if (price < 2000) {
-            throw new PriceLessThanTwoThousand("Price is less than 2000");
+            throw new PriceOutOfRangeException("Price is less than 2000");
         }
 
         if (user.getAge() <= 0) {
-            throw new AgeInvalidException("Age cannot be less than zero");
+            throw new AgeInvalidException("Age cannot be less than or equal to zero");
         }
 
         if(user.getAge() < 10) {
-            throw new AgeLessThanTenException("Your age should be atleast 10");
+            throw new AgeOutOfRangeException("Your age should be atleast 10");
         }
 
         if(user.getAge() < 20) {
-            throw new AgeLessThanTwentyException("Your age should be atleast 20");
+            throw new AgeOutOfRangeException("Your age should be atleast 20");
         }
 
         System.out.println("All good. Go and watch movie");
 
     }
 
-    public static void watch(User user) throws RatingLessThanThreeException{
+    public static void watch(User user) throws RatingOutOfRangeException {
         if (user.getRating() < 3) {
-            throw new RatingLessThanThreeException("Your rating is less than 3");
+            throw new RatingOutOfRangeException("Your rating is less than 3");
         }
 
         System.out.println("Movie watched!!");
